ef016a2ab2bde58cc10055e31a4462e3059b044b
gness1804 <graham.nessler@gmail.com>
2022-05-13 11:00:54 -0500

    all tests passing

--- isbn-verifier.ts
+++ isbn-verifier.ts
@@ -2,7 +2,7 @@ const compute = (arr: string[]): boolean => {
   if (arr.length !== 10) return false;
   let modifiedArr = arr;
   if (arr[9] === 'X') modifiedArr[9] = '10';
-  let modifiedParsedArr: number[] = modifiedArr.map(parseInt);
+  let modifiedParsedArr: number[] = modifiedArr.map(x => parseInt(x, 10))
   if (modifiedParsedArr.find(char => isNaN(char))) return false;
   return (modifiedParsedArr[0] * 10 + modifiedParsedArr[1] * 9 + modifiedParsedArr[2] * 8 + modifiedParsedArr[3] * 7 + modifiedParsedArr[4] * 6 + modifiedParsedArr[5] * 5 + modifiedParsedArr[6] * 4 + modifiedParsedArr[7] * 3 + modifiedParsedArr[8] * 2 + modifiedParsedArr[9] * 1) % 11 === 0
 }
