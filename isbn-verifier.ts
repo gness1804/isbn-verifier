3259f647e410ef3c3e4102ad490cab945570f25c
gness1804 <graham.nessler@gmail.com>
2022-05-13 10:51:25 -0500

    first pass; 10 tests pass

--- isbn-verifier.ts
+++ isbn-verifier.ts
@@ -1,3 +1,14 @@
-export function isValid(isbn: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const compute = (arr: string[]): boolean => {
+  if (arr.length !== 10) return false;
+  let modifiedArr = arr;
+  if (arr[9] === 'X') modifiedArr[9] = '10';
+  let modifiedParsedArr: number[] = modifiedArr.map(parseInt);
+  if (modifiedParsedArr.find(char => isNaN(char))) return false;
+  return (modifiedParsedArr[0] * 10 + modifiedParsedArr[1] * 9 + modifiedParsedArr[2] * 8 + modifiedParsedArr[3] * 7 + modifiedParsedArr[4] * 6 + modifiedParsedArr[5] * 5 + modifiedParsedArr[6] * 4 + modifiedParsedArr[7] * 3 + modifiedParsedArr[8] * 2 + modifiedParsedArr[9] * 1) % 11 === 0
+}
+
+export function isValid(isbn: string): boolean {
+  if (!isbn) return false;
+  const isbnNoHyphens = isbn.replaceAll('-', '');
+  return compute(isbnNoHyphens.split(''))
 }
