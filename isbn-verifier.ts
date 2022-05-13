f170e51be4dcd659e5f999c1ecbf78aec9fe7182
gness1804 <graham.nessler@gmail.com>
2022-05-13 11:09:25 -0500

    refactor

--- isbn-verifier.ts
+++ isbn-verifier.ts
@@ -2,13 +2,18 @@ const compute = (arr: string[]): boolean => {
   if (arr.length !== 10) return false;
   let modifiedArr = arr;
   if (arr[9] === 'X') modifiedArr[9] = '10';
-  let modifiedParsedArr: number[] = modifiedArr.map(x => parseInt(x, 10))
+  let modifiedParsedArr: number[] = arr
+    .map((char, index) => {
+      if (index === 9 && char === 'X') return '10';
+      return char;
+    })
+    .map(char => parseInt(char, 10))
+
   if (modifiedParsedArr.find(char => isNaN(char))) return false;
   return (modifiedParsedArr[0] * 10 + modifiedParsedArr[1] * 9 + modifiedParsedArr[2] * 8 + modifiedParsedArr[3] * 7 + modifiedParsedArr[4] * 6 + modifiedParsedArr[5] * 5 + modifiedParsedArr[6] * 4 + modifiedParsedArr[7] * 3 + modifiedParsedArr[8] * 2 + modifiedParsedArr[9] * 1) % 11 === 0
 }
 
 export function isValid(isbn: string): boolean {
   if (!isbn) return false;
-  const isbnNoHyphens = isbn.replaceAll('-', '');
-  return compute(isbnNoHyphens.split(''))
+  return compute(isbn.replaceAll('-', '').split(''))
 }
